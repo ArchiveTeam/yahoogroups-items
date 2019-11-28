be27236c0966fcebf93f5003410f5410e455a074
Arkiver2 <arkiver@protonmail.com>
2019-11-29 00:47:09 +0100

    Process asdf list.

---
+++ raw/asdf/process.py
@@ -0,0 +1,13 @@
+import os
+
+
+def main():
+    for filename in os.listdir('.'):
+        with open(filename, 'r') as f_in, \
+                open(filename + '.processed', 'w') as f_out:
+            items = {'group:' + s.strip().split(' ', 1)[0] for s in f_in}
+            f_out.write('\n'.join(sorted(items)))
+
+if __name__ == '__main__':
+    main()
+
