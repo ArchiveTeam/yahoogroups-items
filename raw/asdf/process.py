3eddc98badbcee98ba2ce12507147ab191b0031a
Arkiver2 <arkiver@protonmail.com>
2019-11-29 00:48:44 +0100

    Remove bad files

--- raw/asdf/process.py
+++ raw/asdf/process.py
@@ -3,6 +3,8 @@ import os
 
 def main():
     for filename in os.listdir('.'):
+        if not filename.endswith('.txt'):
+            continue
         with open(filename, 'r') as f_in, \
                 open(filename + '.processed', 'w') as f_out:
             items = {'group:' + s.strip().split(' ', 1)[0] for s in f_in}
